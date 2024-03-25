aaa7b14ff117e33ce467516670478ddbffaea4ad
Vitaliy Butsai <150235951+TFatM@users.noreply.github.com>
2024-03-25 16:54:00 +0200

    Add files via upload

--- sushiman/js/swiper.js
+++ sushiman/js/swiper.js
@@ -1,11 +1,13 @@
 var swiper = new Swiper('.swiper', {
   loop: true,
+  grabCursor: true,
+  centeredSlides: true, 
   slidesPerView: 3,
   spaceBetween: 64,
       modules: [myPlugin],
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
       },
       debugger: true,
     });
